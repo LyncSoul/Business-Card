91124cb0da1c87e0a6a1b50bf3e1e6ea0d0b6ed6
LyncSoul <lucas.eqh@hotmail.com>
2022-08-10 00:40:41 -0300

    layout refactoring

--- app/src/main/java/br/com/dio/businesscard/ui/AddBusinessCardActivity.kt
+++ app/src/main/java/br/com/dio/businesscard/ui/AddBusinessCardActivity.kt
@@ -26,11 +26,11 @@ class AddBusinessCardActivity : AppCompatActivity() {
     private fun insertListeners() {
         binding.btnConfirm.setOnClickListener {
             val businessCard = BusinessCard(
-                nome = binding.tilNome.editText?.text.toString(),
-                empresa = binding.tilEmpresa.editText?.text.toString(),
-                telefone = binding.tilTelefone.editText?.text.toString(),
-                email = binding.tilEmail.editText?.text.toString(),
-                fundoPersonalizado = binding.tilCor.editText?.text.toString()
+                nome = binding.tilNome.text.toString(),
+                empresa = binding.tilEmpresa.text.toString(),
+                telefone = binding.tilTelefone.text.toString(),
+                email = binding.tilEmail.text.toString(),
+                fundoPersonalizado = binding.tilCor.text.toString()
             )
             mainViewModel.insert(businessCard)
             Toast.makeText(this, R.string.label_show_success, Toast.LENGTH_SHORT).show()
